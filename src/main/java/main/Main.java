fea9fe9d03773488a413789d92037ac5c4eac911
Nabhoneil Chattopadhyay <nchattopadhyay@cineverse.com>
2025-02-07 13:51:12 +0530

    Mades changes to deploy in staging env

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,7 +2,7 @@ package main.java.main;
 
 public class Main{
 	public static void main(String[] args) {
-		System.out.println("Hello, Maven!");
+		System.out.println("Hello, Maven from staging!");
 	}
 }
 
